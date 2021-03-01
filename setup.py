ced2d66e14e297433566286aa8f706cf128bacdc
Jonathan Cameron <jacc@bu.edu>
2021-03-01 16:47:20 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup
+
+with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name             = 'sevstack',
+    version          = '0.1',
+    description      = 'A ChRIS plugin to produce ranked list of severity scores',
+    long_description = readme,
+    author           = 'Jonathan Cameron',
+    author_email     = 'jacc@bu.edu',
+    url              = 'http://wiki',
+    packages         = ['sevstack'],
+    install_requires = ['chrisapp'],
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+    license          = 'MIT',
+    zip_safe         = False,
+    python_requires  = '>=3.6',
+    entry_points     = {
+        'console_scripts': [
+            'sevstack = sevstack.__main__:main'
+            ]
+        }
+)
---
+++ sevstack/__main__.py
@@ -0,0 +1,10 @@
+from sevstack.sevstack import Sevstack
+
+
+def main():
+    chris_app = Sevstack()
+    chris_app.launch()
+
+
+if __name__ == "__main__":
+    main()
